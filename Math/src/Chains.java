7ac404d05474c4a60262e71f07cd777f765b802a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-23 16:11:01 +0200

    Simple if connecteg with chains

--- Math/src/Chains.java
+++ Math/src/Chains.java
@@ -13,6 +13,20 @@ public class Chains {//Łańcuchy
         System.out.println("Konkatenacja = " + message);
 
         System.out.println("Equals do porównania wartości łańcuchów true lub false"+ expletive.equals(PG13));
+        System.out.println("Porównanie łańcucha z uwzględnienia wielkości znaków = " + "welcome".equals(welcome));
+        System.out.println("Porównanie łańcucha bez uwzględnienia wielkości znaków = " + "welcome".equalsIgnoreCase(welcome));
+
+        //if porównuje łańcuchy
+        if (welcome == "welcome"){
+            System.out.println("Takie same łańcuchy");
+        }else
+            System.out.println("Różne łancuchy");
+
+        //if dwóch wartości
+        if (welcome == null && welcome.length() == 0){
+            System.out.println("Pusy łańcuch");
+        }else
+            System.out.println("Łańcuch zawiera znaki");
     }
 
 }
