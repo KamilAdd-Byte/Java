9bcfe4a869a58f80b2562f62644fe913392cac4a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-23 15:06:03 +0200

    Math.class

---
+++ Math/src/Math.java
@@ -0,0 +1,10 @@
+public class Math {
+    public static void main(String[] args) {
+
+        int x = 456;
+        int y = (int) java.lang.Math.sqrt(x);
+        System.out.println(y);
+
+    }
+
+}
